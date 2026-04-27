253218e21bc2417aaa66463eb588271e65e17037
Misha O'Keeffe <okeeffemsoc@gmail.com>
2026-04-27 12:23:56 -0700

    250 conditions/pptx

--- gen_assignment_list.py
+++ gen_assignment_list.py
@@ -1,7 +1,7 @@
 import random, json
 
 N_CONDITIONS   = 2    # number of conditions
-N_PARTICIPANTS = 300  # set > target N; DataPipe n_conditions must match this
+N_PARTICIPANTS = 250  # set > target N; DataPipe n_conditions must match this
 
 assert N_PARTICIPANTS % N_CONDITIONS == 0, "N_PARTICIPANTS must be divisible by N_CONDITIONS"
 
