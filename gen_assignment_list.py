5d2d835300216f5fcb9ac8774c7173aea8bb7f23
Misha O'Keeffe <okeeffemsoc@gmail.com>
2026-05-26 09:47:48 -0700

    changing to 120 afc

--- gen_assignment_list.py
+++ gen_assignment_list.py
@@ -1,7 +1,7 @@
 import random, json
 
 N_CONDITIONS   = 2    # number of conditions
-N_PARTICIPANTS = 160  # set > target N; DataPipe n_conditions must match this
+N_PARTICIPANTS = 120  # set > target N; DataPipe n_conditions must match this
 
 # full shuffle — repeat conditions to fill N_PARTICIPANTS, then shuffle
 slots = (list(range(N_CONDITIONS)) * (N_PARTICIPANTS // N_CONDITIONS + 1))[:N_PARTICIPANTS]
